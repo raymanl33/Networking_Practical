f804d7f437ed6be78bd7c0688e0e3c42df6352e0
Raymond Lee <raymanl33@icloud.com>
2022-12-07 15:17:12 -0800

    test

---
+++ terraform_practical/main.tf
@@ -0,0 +1,76 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+data "digitalocean_ssh_key" "trf_ssh_key" {
+  name = "ansible_do"
+}
+
+data "digitalocean_project" "lab_project" {
+  name = "4640_Labs"
+}
+
+# Create a new tag 
+# Use tag to create load balancer
+resource "digitalocean_tag" "instanceRole" {
+  name = "A01062029"
+}
+
+
+resource "digitalocean_droplet" "application" {
+  image    = "rockylinux-9-x64"
+  count    = var.droplet_count
+  name     = "application_A01062029"
+  region   = var.region
+  size     = "s-1vcpu-512mb-10gb"
+  tags     = [digitalocean_tag.instanceRole.id]
+  ssh_keys = [data.digitalocean_ssh_key.trf_ssh_key.id]
+  vpc_uuid = digitalocean_vpc.web_vpc.id
+
+  # lifecycle
+  # if terraform is unable to change existing resources or existing infrastructure it will destroy that infrastructure
+  # and create a new one 
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "digitalocean_droplet" "frontend" {
+  image    = "rockylinux-9-x64"
+  count    = var.droplet_count
+  name     = "frontend"
+  region   = var.region
+  size     = "s-1vcpu-512mb-10gb"
+  tags     = [digitalocean_tag.instanceRole.id]
+  ssh_keys = [data.digitalocean_ssh_key.trf_ssh_key.id]
+  vpc_uuid = digitalocean_vpc.web_vpc.id
+
+  # lifecycle
+  # if terraform is unable to change existing resources or existing infrastructure it will destroy that infrastructure
+  # and create a new one 
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+output "server_ip" {
+  value = digitalocean_droplet.application.*.ipv4_address
+}
+
+# Create a new VPC
+resource "digitalocean_vpc" "web_vpc" {
+  name   = "web"
+  region = var.region
+}
