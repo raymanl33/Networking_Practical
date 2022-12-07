118292ac4740f83d3292de0602ea8e37542caaf4
Raymond Lee <raymanl33@icloud.com>
2022-12-07 15:29:08 -0800

    test

--- terraform_practical/variables.tf
+++ terraform_practical/variables.tf
@@ -1,2 +1,14 @@
 # The API token
 variable "do_token" {}
+
+
+# set the default region to sfo3
+variable "region" {
+  type = string
+  default = "sfo3"
+}
+
+variable "image_name" {
+    type = string
+    default = "rockylinux-9-x64"
+}
